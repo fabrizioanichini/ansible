4842a71e041ca8f0fbf3e548fa523258204a9f94
Fabrizio Anichini <fabrizioanichini@proton.me>
2024-09-13 13:07:37 +0200

    show also dotfiles when searching telescope

--- env/.config/nvim/init.lua
+++ env/.config/nvim/init.lua
@@ -403,7 +403,13 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', function()
+        require('telescope.builtin').find_files {
+          hidden = true,
+          respect_gitignore = true,
+          file_ignore_patterns = { '^.git/' },
+        }
+      end, { desc = '[S]earch [F]iles (including dotfiles, respecting .gitignore)' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
